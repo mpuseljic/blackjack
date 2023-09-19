bb0922d09f7e1baab524737b04ae21932f7d9cb3
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-09-19 09:24:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // 2. Use getRandomCard() to set the values of firstCard and secondCard
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -25,6 +24,11 @@ function getRandomCard() {
 
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
@@ -49,10 +53,10 @@ function renderGame() {
 
 
 function newCard() {
-    // 3. Use the getRandomCard() to set the value of card
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    if(isAlive === true && hasBlackJack === false ){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+}
 }
